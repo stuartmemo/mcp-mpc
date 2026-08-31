5073dab33d8f2f9c39f8dba881cf989cf7c36010
Stuart Memo <stuartmemo@gmail.com>
2026-08-31 17:05:01 +0100

    Finalize Sites SPA routing

--- src/sites-worker.ts
+++ src/sites-worker.ts
@@ -8,35 +8,8 @@ interface SitesEnvironment {
 
 const worker = {
   async fetch(request: Request, environment: SitesEnvironment): Promise<Response> {
-    let response = await environment.ASSETS.fetch(request)
-
-    if (
-      response.status !== 404 ||
-      request.method !== 'GET' ||
-      !(request.headers.get('accept') ?? '').includes('text/html')
-    ) {
-      return withSiteOrigin(response, request)
-    }
-
-    const indexUrl = new URL('/index.html', request.url)
-    response = await environment.ASSETS.fetch(new Request(indexUrl, request))
-    return withSiteOrigin(response, request)
+    return environment.ASSETS.fetch(request)
   },
-}
-
-async function withSiteOrigin(response: Response, request: Request): Promise<Response> {
-  if (!(response.headers.get('content-type') ?? '').includes('text/html')) {
-    return response
-  }
-
-  const origin = new URL(request.url).origin
-  const html = (await response.text()).replaceAll('__SITE_ORIGIN__', origin)
-
-  return new Response(html, {
-    headers: response.headers,
-    status: response.status,
-    statusText: response.statusText,
-  })
 }
 
 export default worker
