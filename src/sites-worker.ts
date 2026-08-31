48e6edd6cc8218d2e7d3d6a9feef146149707f22
Stuart Memo <stuartmemo@gmail.com>
2026-08-31 16:28:37 +0100

    Configure private Sites deployment

--- src/sites-worker.ts
+++ src/sites-worker.ts
@@ -8,19 +8,35 @@ interface SitesEnvironment {
 
 const worker = {
   async fetch(request: Request, environment: SitesEnvironment): Promise<Response> {
-    const response = await environment.ASSETS.fetch(request)
+    let response = await environment.ASSETS.fetch(request)
 
     if (
       response.status !== 404 ||
       request.method !== 'GET' ||
       !(request.headers.get('accept') ?? '').includes('text/html')
     ) {
-      return response
+      return withSiteOrigin(response, request)
     }
 
     const indexUrl = new URL('/index.html', request.url)
-    return environment.ASSETS.fetch(new Request(indexUrl, request))
+    response = await environment.ASSETS.fetch(new Request(indexUrl, request))
+    return withSiteOrigin(response, request)
   },
+}
+
+async function withSiteOrigin(response: Response, request: Request): Promise<Response> {
+  if (!(response.headers.get('content-type') ?? '').includes('text/html')) {
+    return response
+  }
+
+  const origin = new URL(request.url).origin
+  const html = (await response.text()).replaceAll('__SITE_ORIGIN__', origin)
+
+  return new Response(html, {
+    headers: response.headers,
+    status: response.status,
+    statusText: response.statusText,
+  })
 }
 
 export default worker
